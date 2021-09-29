5454a1a8fb7ff8cc9551bc387ac87a1bc63f6a81
codewithnaren <91406174+codewithnaren@users.noreply.github.com>
2021-09-29 20:07:47 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const url = 'https://codewithnaren.github.io/socialandpoliticallife1/Polity6.pdf';
+const url = 'https://codewithnaren.github.io/ThemesinIndianHistory3/ThemesinIndianHistory3.pdf';
 
 
 let pdfDoc = null,
